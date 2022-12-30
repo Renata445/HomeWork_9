10fdccc064df8d3de7e508f07cfc7daba624c2d4
Renata445 <118470394+Renata445@users.noreply.github.com>
2022-12-30 21:03:27 +0300

    Task_64

---
+++ Task_64/Program.cs
@@ -0,0 +1,22 @@
+﻿void NaturalNumbers(int M, int N)
+{
+    int start = M;
+	int end = N;
+	if(M > N)
+	{
+	    start = N;
+		end = M;
+    }
+    for(int i = end; i >= start; i--)
+    {
+        Console.Write($"{i} ");
+    }
+}
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Введите второе число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+NaturalNumbers(M, N);
