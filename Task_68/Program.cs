b2f42f0b66b9bcf979cbdc114e214846b31b7380
Renata445 <118470394+Renata445@users.noreply.github.com>
2022-12-30 21:03:41 +0300

    Task_68

---
+++ Task_68/Program.cs
@@ -0,0 +1,27 @@
+﻿void AkkermanNumbers(int M, int N)
+{
+    Console.Write(Akkerman(M, N)); 
+}
+
+int Akkerman(int M, int N)
+{
+    if (M == 0)
+    {
+        return N + 1;
+    }
+    else if (N == 0 && M > 0)
+    {
+        return Akkerman(M - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(M - 1, Akkerman(M, N - 1)));
+    }
+}
+
+Console.Clear();
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+AkkermanNumbers(M,N);
