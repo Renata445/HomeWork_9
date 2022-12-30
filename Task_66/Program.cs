17ce5fd7cbd9ff66380e32704407588bd0fd1827
Renata445 <118470394+Renata445@users.noreply.github.com>
2022-12-30 21:03:34 +0300

    Task_66

---
+++ Task_66/Program.cs
@@ -0,0 +1,24 @@
+﻿void SumNumbers(int M, int N)
+{
+    int sum = 0;
+    int start = M;
+	int end = N;
+	if(M > N)
+	{
+	    start = N;
+		end = M;
+    }
+    for(int i = start; i <= end; i++)
+    {
+        sum += i;
+    }
+    Console.WriteLine($"Сумма = {sum}");
+}
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Введите второе число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+SumNumbers(M, N);
